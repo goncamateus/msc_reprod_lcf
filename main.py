26bf0fb321919e23d96b269696be3460a19193b5
goncamateus <mgm4@cin.ufpe.br>
2020-06-28 20:36:52 -0700

    thats the one

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ def main(dataset, test_only=False, dec=True):
     regressor = create_model(input_shape=(X_train.shape[1], X_train.shape[2]),
                              lr=1e-3)
     if not test_only:
-        regressor.fit(X_train, y_train, epochs=100,
-                      batch_size=128, callbacks=get_callbacks(central))
+        regressor.fit(X_train, y_train, epochs=10,
+                      batch_size=32, callbacks=get_callbacks(central))
     else:
         load_model(regressor, central)
 
